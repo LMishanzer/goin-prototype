b004f6f63dad05feb0096f11a9f01c1ed29a32cc
Dima <9165135-vizeldim@users.noreply.gitlab.com>
2023-11-26 22:19:26 +0100

    Clickable CategoryCard & Close Overylay BTN

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,15 +1,23 @@
 import { FC } from "react";
-import styles from "./CategoryCard.module.css";
+import { Button, styled } from "@mui/material";
 
 interface CategoryCardProps {
   name: string;
   color: string;
 }
 
+export const CardButton = styled(Button)({
+  color: "#fff",
+  fontWeight: "800",
+  borderRadius: "22px",
+  outline: "thin solid rgba(255, 255, 255, .7)",
+  minWidth: "170px",
+  minHeight: "100px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
 export const CategoryCard: FC<CategoryCardProps> = ({ name, color }) => {
-  return (
-    <div style={{ background: color }} className={styles.categoryCard}>
-      {name}
-    </div>
-  );
+  return <CardButton style={{ background: color }}>{name}</CardButton>;
 };
