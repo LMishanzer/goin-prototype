ac5cf05cac742592a4205b9f0919536ee201c0f8
Dima <9165135-vizeldim@users.noreply.gitlab.com>
2023-11-27 00:15:22 +0100

    Add SearchHeader component

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,28 @@
 import { FC } from "react";
+import { useSearchParams } from "react-router-dom";
+import { ListArtist } from "../components/ListArtist";
+import { Footer } from "../templates/Footer";
+import { GoinHeader } from "../templates/GoinHeader";
+import { SearchHeader } from "../templates/SearchHeader";
 
 export const PageSearch: FC = () => {
-  return <div>search</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get("query");
+
+  return (
+    <div
+      style={{
+        background: "#0C1326",
+      }}
+    >
+      <SearchHeader />
+      <div className={"content-wrapper"}>
+        <div>
+          <ListArtist />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
--- src/templates/SearchHeader.tsx
+++ src/templates/SearchHeader.tsx
@@ -1,13 +1,28 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./GoinHeader.module.css";
 import { Button } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import hStyles from "./Header.module.css";
 
+export const SearchHeader: FC<SearchHeaderParams> = () => {
+  const [searchParams] = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState<string>(
+    searchParams.get("query") ?? ""
+  );
 
-export const GoinHeader: FC = () => {
   const navigate = useNavigate();
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      navigate(`/search?query=${encodeURIComponent(searchQuery)}`);
+    }
+  };
 
   const goBack = () => {
     navigate(-1);
@@ -20,6 +35,11 @@ export const GoinHeader: FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.headerContent + " content-wrapper"}>
+        <div className={styles.logoContainer}>
+          <Link className={styles.mainLink} to="/">
+            <h1 className={styles.logo}>GOIN</h1>
+          </Link>
+        </div>
         <div className={styles.navigation}>
           <Button
             sx={{ color: "#fff" }}
@@ -35,12 +55,18 @@ export const GoinHeader: FC = () => {
           >
             <ChevronRightIcon />
           </Button>
+
+          <section>
+            <input
+              placeholder="Search for artist, event, genre..."
+              className={hStyles.searchInput}
+              value={searchQuery}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+          </section>
         </div>
-        <div className={styles.logoContainer}>
-          <Link className={styles.mainLink} to="/">
-            <h1 className={styles.logo}>GOIN</h1>
-          </Link>
-        </div>
+
         <div className={styles.options}>
           <Button sx={{ fontWeight: "600" }} color="custom">
             ENG
