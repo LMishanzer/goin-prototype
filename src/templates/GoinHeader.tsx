3299d9d40d564dbf355138580b5a9054e56cdc60
Dima <9165135-vizeldim@users.noreply.gitlab.com>
2023-11-26 23:15:07 +0100

    Add spotify-icon to main nav | [GoinHeader] change to react-router-dom navigation

--- src/templates/GoinHeader.tsx
+++ src/templates/GoinHeader.tsx
@@ -1,16 +1,38 @@
 import { FC } from "react";
 import styles from "./GoinHeader.module.css";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 export const GoinHeader: FC = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goForward = () => {
+    navigate(1);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.navigation}>
-        <a href="javascript:history.back()" className={styles.navbutton}>
-          &#8249;
-        </a>
-        <a href="javascript:history.forward()" className={styles.navbutton}>
-          &#8250;
-        </a>
+        <Button
+          sx={{ color: "#fff" }}
+          onClick={goBack}
+          className={styles.navbutton}
+        >
+          <ChevronLeftIcon />
+        </Button>
+        <Button
+          sx={{ color: "#fff" }}
+          onClick={goForward}
+          className={styles.navbutton}
+        >
+          <ChevronRightIcon />
+        </Button>
       </div>
       <div className={styles.logoContainer}>
         <h1 className={styles.logo}>GOIN</h1>
