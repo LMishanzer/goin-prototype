37396fe5300507d87c4f445d601a6d3e802ad55b
Dima <9165135-vizeldim@users.noreply.gitlab.com>
2023-11-27 07:45:24 +0100

    [EventPage] consistent paddings

--- src/pages/EventDetail.tsx
+++ src/pages/EventDetail.tsx
@@ -1,17 +1,17 @@
 import data from "../data/events.json";
-import {GoinHeader} from "../templates/GoinHeader.tsx";
-import {Footer} from "../templates/Footer.tsx";
-import {Link as RouterLink, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import styles from "./EventDetail.module.css"
-import {Button, Stack} from "@mui/material";
-import {ChevronRight} from "@mui/icons-material";
-import {CardArtist} from "../components/CardArtist.tsx";
+import { GoinHeader } from "../templates/GoinHeader.tsx";
+import { Footer } from "../templates/Footer.tsx";
+import { Link as RouterLink, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styles from "./EventDetail.module.css";
+import { Button, Stack } from "@mui/material";
+import { ChevronRight } from "@mui/icons-material";
+import { CardArtist } from "../components/CardArtist.tsx";
 
 type Event = (typeof data)[0];
 
 export const EventDetail = () => {
-  const {eventId} = useParams();
+  const { eventId } = useParams();
   const [event, setArtist] = useState<Event>();
 
   useEffect(() => {
@@ -20,61 +20,88 @@ export const EventDetail = () => {
   }, [eventId]);
 
   return event ? (
-    <div>
-      <GoinHeader/>
-      <div className={styles.detailsContainer}>
-        <h1 style={{
-            fontSize: "40px"
+    <div style={{ background: "#09111C" }}>
+      <GoinHeader />
+      <div className={styles.detailsContainer + " content-wrapper"}>
+        <h1
+          style={{
+            fontSize: "40px",
           }}
         >
           {event.name}
         </h1>
         <div className={styles.infoContainer}>
           <ul>
-            <li><span className={styles.infoProp}>Address:</span>Výstaviště Praha, Holešovice, Prague</li>
-            <li><span className={styles.infoProp}>When:</span>{event.when}</li>
-            <li><span className={styles.infoProp}>Entry:</span>CZK 600–2600</li>
+            <li>
+              <span className={styles.infoProp}>Address:</span>Výstaviště Praha,
+              Holešovice, Prague
+            </li>
+            <li>
+              <span className={styles.infoProp}>When:</span>
+              {event.when}
+            </li>
+            <li>
+              <span className={styles.infoProp}>Entry:</span>CZK 600–2600
+            </li>
           </ul>
           <ul>
-            <li><span className={styles.infoProp}>Venue:</span>{event.venue}</li>
-            <li><span className={styles.infoProp}>Venue's website:</span><a href="https://www.vystavistepraha.eu/">www.vystavistepraha.eu</a></li>
+            <li>
+              <span className={styles.infoProp}>Venue:</span>
+              {event.venue}
+            </li>
+            <li>
+              <span className={styles.infoProp}>Venue's website:</span>
+              <a href="https://www.vystavistepraha.eu/">
+                www.vystavistepraha.eu
+              </a>
+            </li>
           </ul>
         </div>
-        <Button style={{
-          backgroundColor: "#0F4EA6",
-          color: "#fff",
-          marginTop: "80px",
-          padding: "15px 10px 15px 20px",
-          borderRadius: "8px",
-          fontWeight: "bold"
-        }}>
+        <Button
+          style={{
+            backgroundColor: "#0F4EA6",
+            color: "#fff",
+            marginTop: "80px",
+            padding: "15px 10px 15px 20px",
+            borderRadius: "8px",
+            fontWeight: "bold",
+          }}
+        >
           Buy ticket
-          <ChevronRight style={{
-              marginLeft: "50px"
-          }}/>
+          <ChevronRight
+            style={{
+              marginLeft: "50px",
+            }}
+          />
         </Button>
-        <div style={{
-          marginTop: "100px"
-        }}>
+        <div
+          style={{
+            marginTop: "100px",
+          }}
+        >
           <h2 style={{ fontSize: "30px" }}>Performing artists</h2>
           <Stack
-              direction="row"
-              spacing={4}
-              useFlexGap
-              justifyContent="space-between"
-              marginTop="40px"
+            direction="row"
+            spacing={4}
+            useFlexGap
+            justifyContent="space-between"
+            marginTop="40px"
           >
             {event.artists.map((artist) => (
-                <RouterLink to={`/event-detail/${artist.id}`} key={artist.id}>
-                  <CardArtist name={artist.name} img={artist.image} key={artist.id}/>
-                </RouterLink>
+              <RouterLink to={`/event-detail/${artist.id}`} key={artist.id}>
+                <CardArtist
+                  name={artist.name}
+                  img={artist.image}
+                  key={artist.id}
+                />
+              </RouterLink>
             ))}
           </Stack>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   ) : (
-      <div>NOT FOUND</div>
+    <div>NOT FOUND</div>
   );
-}
+};
